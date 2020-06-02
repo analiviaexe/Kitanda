07f5d50d8a7d20aeb1e8fd1c4df7350dddbb1a32
Ana Livia <analivia.exe@gmail.com>
2020-06-02 17:23:44 -0300

    Trying set params to products

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,19 @@
-// Empty JS for your own code to be here
+function precoSabao(){
+    const myRequest = new Request('./src/images/sabao.png');
+
+    const myURL = myRequest.url; // http://localhost/flowers.jpg
+    const myMethod = myRequest.method; // GET
+    const myCred = myRequest.credentials; // omit
+}
+
+function precoCerveja(){
+
+}
+
+function precoChocolate(){
+
+}
+
+function precoArroz(){
+
+}
